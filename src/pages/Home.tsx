ac317e21fac7633fe210585ae1d288820da558f9
Mohamed El-Sharqawy <dev.elbehery@gmail.com>
2024-07-07 19:19:37 +0300

    done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,35 +8,30 @@ export default function Home() {
   const { activeVideo, setActiveVideo, setIsCourseContentHidden } = useStore();
 
   return (
-    <>
-      <div className="flex justify-center gap-x-6">
-        <main className="w-full sm:flex sm:items-center sm:justify-center h-screen">
-          {/* Large Screens */}
-          <ReactPlayer
-            width={"100%"}
-            height={"80vh"}
-            url={activeVideo.url}
-            playsinline
-            controls
-            onEnded={() => {
-              const nextVideo = getNextVideo(activeVideo.id - 1);
-              if (nextVideo) setActiveVideo(nextVideo);
-            }}
-          />
-        </main>
+    <article className="flex justify-center gap-x-6">
+      <main className="w-full sm:flex sm:items-center sm:justify-center h-screen">
+        {/* Large Screens */}
+        <ReactPlayer
+          width={"100%"}
+          height={"80vh"}
+          url={activeVideo.url}
+          playsinline
+          controls
+          onEnded={() => {
+            const nextVideo = getNextVideo(activeVideo.id - 1);
+            if (nextVideo) setActiveVideo(nextVideo);
+          }}
+        />
+      </main>
 
-        <div
-          onClick={() => setIsCourseContentHidden(false)}
-          className="fixed top-0 sm:top-[10vh] -right-32 bg-[#2D2F31] border p-4 flex items-center gap-x-3 hover:bg-[#3c3e41] hover:-right-1 transition-all duration-500 cursor-pointer"
-        >
-          <RxHamburgerMenu size={23} /> Course Content
-        </div>
-
-        <Sidebar />
+      <div
+        onClick={() => setIsCourseContentHidden(false)}
+        className="fixed top-0 sm:top-[10vh] -right-32 bg-[#2D2F31] border p-4 flex items-center gap-x-3 hover:bg-[#3c3e41] hover:-right-1 transition-all duration-500 cursor-pointer"
+      >
+        <RxHamburgerMenu size={23} /> Course Content
       </div>
-    </>
+
+      <Sidebar />
+    </article>
   );
 }
-
-// mttv_end_client
-// fhwwuf[]h12
